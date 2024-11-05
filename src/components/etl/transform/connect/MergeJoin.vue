1f28ea9d9663b539381df6ecfd776b2432b5ccfc
jesse han <jesse.18@163.com>
2024-11-05 22:57:02 +0800

    合并结果集组件前序步骤加载时使用字符串内存替换对象内容

--- src/components/etl/transform/connect/MergeJoin.vue
+++ src/components/etl/transform/connect/MergeJoin.vue
@@ -3,8 +3,8 @@
     <q-form class="row q-col-gutter-xs">
       <q-input class="col-12 col-md-6" outlined v-model="form.name" :label="$t('form.mergeJoin.name')" :rules="[ val => val && val.length > 0 || 'Please type something']" hint=""/>
       <q-select class="col-12 col-md-6" clearable outlined v-model="form.join_type" :options="joinTypes" filled :label="$t('form.mergeJoin.join')"/>
-      <q-select class="col-12 col-md-6" clearable outlined v-model="form.step1" :options="previousSteps" emit-value map-options :label="$t('form.mergeJoin.step1')" hint="" @input="selectStep1($event)"/>
-      <q-select class="col-12 col-md-6" clearable outlined v-model="form.step2" :options="previousSteps" emit-value map-options :label="$t('form.mergeJoin.step2')" hint="" @input="selectStep2($event)"/>
+      <q-select class="col-12 col-md-6" clearable outlined v-model="form.step1" :options="previousSteps" :label="$t('form.mergeJoin.step1')" hint="" @input="selectStep1($event)"/>
+      <q-select class="col-12 col-md-6" clearable outlined v-model="form.step2" :options="previousSteps" :label="$t('form.mergeJoin.step2')" hint="" @input="selectStep2($event)"/>
       <q-table class="col-12" :data="form.fieldMappingData" :columns="fieldMappingColumns" :rows-per-page-options="[0]" row-key="name" separator="cell" hide-bottom :title="$t('form.mergeJoin.mergeField')">
         <template v-slot:top-right>
           <q-btn-dropdown split outline color="primary" icon="add" @click="addFieldMapping">
@@ -173,7 +173,7 @@ export default {
         if (step.depth === 0) {
           const ext = JSON.parse(step.ext)
           if (ext.sourceFields && ext.sourceFields.length > 0) {
-            vm.previousSteps.push({ value: step.title, label: step.title })
+            vm.previousSteps.push(step.title)
             vm.stepFields[step.title] = []
             ext.sourceFields.forEach(field => {
               vm.stepFields[step.title].push(field)
@@ -215,7 +215,7 @@ export default {
         })
       })
     }
-    
+
     const mxCellValue = vm.$store.getters['etl/getMxCellForm']
     if (mxCellValue) {
       vm.form = Object.assign(vm.form, mxCellValue)
